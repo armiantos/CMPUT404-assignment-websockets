ecd70e9e9e0a7e5737a49a84495387fa5578cf8c
Armianto Sumitro <armiantos@gmail.com>
2022-03-30 14:02:02 -0600

    Format sockets.py

--- sockets.py
+++ sockets.py
@@ -27,24 +27,26 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+
 class World:
+
     def __init__(self):
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+
     def add_set_listener(self, listener):
-        self.listeners.append( listener )
+        self.listeners.append(listener)
 
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def set(self, entity, data):
         self.space[entity] = data
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def update_listeners(self, entity):
         '''update the set listeners'''
@@ -55,23 +57,28 @@ class World:
         self.space = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
-    
+        return self.space.get(entity, dict())
+
     def world(self):
         return self.space
 
-myWorld = World()        
 
-def set_listener( entity, data ):
+myWorld = World()
+
+
+def set_listener(entity, data):
     ''' do something with the update ! '''
     pass
 
-myWorld.add_set_listener( set_listener )
-        
+
+myWorld.add_set_listener(set_listener)
+
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect('static/index.html')
+
 
 def read_ws(ws: WebSocket, client):
     '''A greenlet function that reads from the websocket and updates the world'''
@@ -84,22 +91,24 @@ def read_ws(ws: WebSocket, client):
         myWorld.set(name, entity_object[name])
     return None
 
+
 @sockets.route('/subscribe')
 def subscribe_socket(ws: WebSocket):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
+
     # XXX: TODO IMPLEMENT ME
     def listener(entity, data):
         if ws.closed:
             return
         body = json.dumps({entity: data})
         ws.send(body)
+
     myWorld.add_set_listener(listener)
 
     # TODO: Use gevents
     while not ws.closed:
         read_ws(ws, None)
-
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -114,7 +123,8 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
-@app.route("/entity/<entity>", methods=['POST','PUT'])
+
+@app.route("/entity/<entity>", methods=['POST', 'PUT'])
 def update(entity):
     '''update the entities via this interface'''
     data = flask_post_json()
@@ -122,23 +132,24 @@ def update(entity):
         myWorld.update(entity, key, data[key])
     return data
 
-@app.route("/world", methods=['POST','GET'])    
+
+@app.route("/world", methods=['POST', 'GET'])
 def world():
     '''you should probably return the world here'''
     return json.dumps(myWorld.world())
 
-@app.route("/entity/<entity>")    
+
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
     return json.dumps(myWorld.get(entity))
 
 
-@app.route("/clear", methods=['POST','GET'])
+@app.route("/clear", methods=['POST', 'GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
     return json.dumps(myWorld.world())
-
 
 
 if __name__ == "__main__":
